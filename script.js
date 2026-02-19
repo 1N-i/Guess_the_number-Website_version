24c08b6ea5e2bc766f6912029bb0e69455da0b6b
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-02-19 16:06:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-var secretNumber = parseInt(Math.random() * 51);
-var tries = 5;
-var elementResult = document.getElementById("result");
-elementResult.innerHTML = "A number was choose between 0 and 50";
+let secretNumber = parseInt(Math.random() * 51);
+let tries = 5;
+const elementResult = document.getElementById("result");
+elementResult.innerHTML = "A number was choosen between 0 and 50.\nYou have 5 guesses";
 
 function Guess() {
-  var elementResult = document.getElementById("result");
-  var guess = parseInt(document.getElementById("value").value);
+  let guess = parseInt(document.getElementById("value").value);
+
   if (tries > 0 && guess == secretNumber) {
-    elementResult.innerHTML = "Right answer, new number sorted";
+    elementResult.innerHTML = "Right answer, new number sorted.\nYou have 5 tries";
     secretNumber = parseInt(Math.random() * 51);
     tries = 5;
-  } else if ((tries > 0 && guess >= 31) || guess <= -1) {
+  }
+  else if (tries != 0 && (guess > 50 || guess < 0)) {
     elementResult.innerHTML = "Please put a number between 0 and 50";
-  } else if (tries == 1) {
+  }
+  else if (tries == 1) {
     secretNumber = parseInt(Math.random() * 51);
     tries = 5;
-    elementResult.innerHTML = "New sorted number, you have 5 guesses";
-  } else if (guess > secretNumber) {
+    elementResult.innerHTML = "You are out of tries. A new number was sorted,\nyou have 5 guesses";
+  }
+  else if (guess > secretNumber) {
     tries -= 1;
-    elementResult.innerHTML = "The number is lower than your guess, you have " +
-      tries + " guesses left";
-  } else if (guess < secretNumber) {
+    elementResult.innerHTML = `The number is lower than your guess,\nyou have ${tries} guesses left`;
+  }
+  else if (guess < secretNumber) {
     tries -= 1;
-    elementResult.innerHTML = "The number is higher than your guess, you have " +
-      tries + " guesses left";
+    elementResult.innerHTML = `The number is higher than your guess,\nyou have ${tries} guesses left`;
   }
 }
